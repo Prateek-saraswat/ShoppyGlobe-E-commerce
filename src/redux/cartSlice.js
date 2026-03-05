205c861214e0c6b46303abbac90a0832eaa6c210
Prateek Saraswat <saraswatprateek0@gmail.com>
2026-03-06 02:32:50 +0530

    added relevent comment in cartSlice of redux

--- src/redux/cartSlice.js
+++ src/redux/cartSlice.js
@@ -8,7 +8,7 @@ const cartSlice = createSlice({
     cartItems: [], 
   },
   reducers: {
-
+     //add to cart functionality 
     addToCart: (state, action) => {
       const existingItem = state.cartItems.find(
         (item) => item.id === action.payload.id
@@ -19,27 +19,27 @@ const cartSlice = createSlice({
         state.cartItems.push({ ...action.payload, quantity: 1 });
       }
     },
-
+    //remove from cart
     removeFromCart: (state, action) => {
       state.cartItems = state.cartItems.filter(
         (item) => item.id !== action.payload
       );
     },
-
+    //for increasing quantity
     increaseQuantity: (state, action) => {
       const item = state.cartItems.find((item) => item.id === action.payload);
       if (item) {
         item.quantity += 1;
       }
     },
-
+    //for decreasing quantity 
     decreaseQuantity: (state, action) => {
       const item = state.cartItems.find((item) => item.id === action.payload);
       if (item && item.quantity > 1) {
         item.quantity -= 1;
       }
     },
-
+    //clearing the whole cart
     clearCart: (state) => {
       state.cartItems = [];
     },
